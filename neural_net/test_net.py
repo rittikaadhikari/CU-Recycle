2bc37e6e15d549e995793c39b4a16592498334e4
Sahil Modi <smodi9@illinois.edu>
2018-09-29 18:32:04 -0500

    updated model training

---
+++ neural_net/test_net.py
@@ -0,0 +1,41 @@
+import keras
+from keras.models import load_model
+import argparse
+import os
+import cv2
+import numpy as np
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--dir', required=True, type=str, help='directory to images')
+ap.add_argument('--model', type=str, required=True, help="path to model")
+args = ap.parse_args()
+
+
+model = load_model(args.model)
+print("Model loaded.")
+total_predictions_count = [0, 0]
+inc_predictions_count = [0, 0]
+class_index_dict = {"trash":0, "recycle":1}
+class_val_dict = {"trash":0.0, "recycle":0.0}
+if args.dir[-1] != '/':
+    args.dir += '/'
+for(dirpath, dirnames, filenames) in os.walk(args.dir):
+    for f in filenames:
+        class_str = dirpath.split('/')[-1]
+        img_path = args.dir + class_str + '/' + f
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (224, 224))
+        img = np.expand_dims(img, axis=0)
+        total_predictions_count[class_index_dict[class_str]] += 1
+        prediction = model.predict(img)
+        class_val_dict["trash"] = "{0:.3f}".format(prediction[0][0])
+        class_val_dict["recycle"] = "{0:.3f}".format(prediction[0][1])
+        if class_val_dict[class_str] != max(class_val_dict["trash"], class_val_dict["recycle"]):
+            print("Incorrect " + class_str + ": " + f)
+            inc_predictions_count[class_index_dict[class_str]] += 1
+
+print("--------")
+print("Model:", args.model)
+print("Trash Accuracy:", (1 - (inc_predictions_count[0]/total_predictions_count[0])) * 100)
+print("Recycling Accuracy:", (1 - (inc_predictions_count[1]/total_predictions_count[1])) * 100)
--- neural_net/train_net.py
+++ neural_net/train_net.py
@@ -6,10 +6,10 @@ from keras.engine import InputLayer
 from keras.layers import Activation
 from keras.layers.core import Dense, Flatten, Dropout
 from keras.optimizers import Adam
-from keras.metrics import binary_crossentropy
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from matplotlib import pyplot as plt
 import itertools
 import os
@@ -21,11 +21,10 @@ train_path = 'dataset/train'
 valid_path = 'dataset/val'
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--dont_save', action='store_true', help='save model to disk')
 ap.add_argument('--verbose', action='store_true', help='verbose output')
 ap.add_argument('--epochs', type=int, default=5)
 ap.add_argument('--batch_size', type=int, default=15)
-ap.add_argument('--freeze', type=int, help="number of layers to freeze, max = 25", default=22)
+ap.add_argument('--unfreeze', type=int, help="number of layers to unfreeze, max = 25", default=5)
 args = ap.parse_args()
 
 train_datagen = ImageDataGenerator(rotation_range=5,
@@ -49,12 +48,12 @@ for layer in vgg16_model.layers[:-1]:
 
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(.5))
-model.add(Dense(3, activation='sigmoid'))
+model.add(Dense(2, activation='sigmoid'))
 
 num_layers = len(model.layers)
 frozen_count = 0
 for layer in model.layers:
-    if frozen_count < args.freeze:
+    if frozen_count < num_layers - args.unfreeze:
         layer.trainable = False
         frozen_count += 1
 
@@ -62,7 +61,7 @@ if(args.verbose):
     #DEBUG:look at model
     model.summary()
     print("number of layers="+str(num_layers))
-    print("number of frozen="+str(args.freeze))
+    print("number of frozen="+str(num_layers - args.unfreeze))
 
 #compile model with Adam optimization function (learning rate = .0001)
 model.compile(Adam(lr=.0001), loss='binary_crossentropy', metrics=['accuracy'])
@@ -73,14 +72,16 @@ counter = Counter(train_generator.classes)
 max_val = float(max(counter.values()))
 class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
 
-steps = 1673 // args.batch_size
-history = model.fit_generator(train_generator, steps_per_epoch=steps, validation_data=valid_batches, validation_steps=109, epochs=args.epochs, verbose=1, class_weight=class_weights)
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=True, save_best_only=True, save_weights_only=False, mode='auto', period=True)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, verbose=1, mode='auto')
 
-if(not args.dont_save):
-    val_acc = float(history.history["val_acc"][-1])
-    val_acc = "{0:.3f}".format(val_acc)
-    save_label = '_e' + str(args.epochs) + "_va" + str(val_acc)
-    save_label = save_label + '_unfr' + str(args.freeze)
-    save_label = "model" + save_label + ".h5"
-    print("Saving model to", save_label)
-    model.save(save_label)
+steps = 1673 // args.batch_size
+history = model.fit_generator(train_generator, steps_per_epoch=steps, validation_data=valid_batches, validation_steps=109, epochs=args.epochs, verbose=1, class_weight=class_weights, callbacks = [checkpoint, early])
+
+val_acc = float(history.history["val_acc"][-1])
+val_acc = "{0:.3f}".format(val_acc)
+save_label = '_e' + str(args.epochs) + "_va" + str(val_acc)
+save_label = save_label + '_unfr' + str(args.unfreeze)
+save_label = "model16" + save_label + ".h5"
+print("Saving model to", save_label)
+model.save(save_label)
